ccd2bca739cde8523b432499ce44b97c895d932c
Peter van Tol <Peter.van.Tol@witteveenbos.com>
2023-02-27 23:06:21 +0100

    Support auto-detect for install_driver

---
+++ src/litexcnc_toolerator/driver/__init__.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+# Get all .c-files and .h-files. For finer granularity one can also draft this
+# list by hand, but this is not recommended.
+TYPES = ('**/*.c', '**/*.h') # the tuple of file types
+FILES = []
+EXCLUDE = []
+for type_ in TYPES:
+    for file in Path(__file__).parent.glob(type_):
+        if not file.name in EXCLUDE:
+            FILES.append(file)
